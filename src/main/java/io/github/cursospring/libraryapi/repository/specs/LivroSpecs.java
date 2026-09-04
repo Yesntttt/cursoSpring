8ce69fb6fc00860a24b0fd73e1521081c4e205ab
Gabriel <gabrielpiierri.nunes@gmail.com>
2026-09-04 12:05:32 -0300

    04/09/2026 niver da mana

--- src/main/java/io/github/cursospring/libraryapi/repository/specs/LivroSpecs.java
+++ src/main/java/io/github/cursospring/libraryapi/repository/specs/LivroSpecs.java
@@ -1,6 +1,9 @@
 package io.github.cursospring.libraryapi.repository.specs;
 
+import io.github.cursospring.libraryapi.model.GeneroLivro;
 import io.github.cursospring.libraryapi.model.Livro;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 
 public class LivroSpecs {
@@ -12,5 +15,27 @@ public class LivroSpecs {
     public static Specification<Livro> tituloLike(String titulo) {
         return (root, query, cb)
                 -> cb.like(cb.upper(root.get("titulo")), "%" + titulo.toUpperCase() + "%");
+    }
+
+    public static Specification<Livro> generoEqual(GeneroLivro genero) {
+        return (root, query, cb)
+                -> cb.equal(root.get("genero"), genero);
+    }
+
+    public static Specification<Livro> anoPublicacaoEqual(Integer anoPublicacao) {
+        // and to_char(data_publicacao, 'YYYY') = :anoPublicacao
+        return (root, query, cb)
+                -> cb.equal(cb.function("to_char", String.class,
+                root.get("dataPublicacao"), cb.literal("YYYY")), anoPublicacao.toString());
+    }
+
+    public static Specification<Livro> nomeAutorLike(String nome) {
+        return (root, query, cb) -> {
+            Join<Object, Object> joinAutor = root.join("autor", JoinType.INNER);
+
+            return cb.like(cb.upper(joinAutor.get("nome")), "%" + nome + "%");
+
+//            return cb.like(cb.upper(root.get("autor").get("nome")), "%" + nome.toUpperCase() + "%");
+        };
     }
 }
--- src/main/java/io/github/cursospring/libraryapi/service/LivroService.java
+++ src/main/java/io/github/cursospring/libraryapi/service/LivroService.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static io.github.cursospring.libraryapi.repository.specs.LivroSpecs.*;
+
 @Service
 @RequiredArgsConstructor
 public class LivroService {
@@ -30,9 +32,44 @@ public class LivroService {
         repository.delete(livro);
     }
 
-    public List<Livro> pesquisa(String isbn, String nomeAutor,
-                                GeneroLivro genero, Integer anoPublicacao) {
+    public List<Livro> pesquisa(
+            String isbn, String titulo, String nomeAutor, GeneroLivro genero, Integer anoPublicacao) {
 
-        return repository.findAll(LivroSpecs.isbnEqual(isbn));
+//        Specification<Livro> specs = Specification
+//                .where(LivroSpecs.isbnEqual(isbn))
+//                .and(LivroSpecs.tituloLike(titulo))
+//                .and(LivroSpecs.generoEqual(genero));
+
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction());
+
+        if(isbn != null) {
+            specs = specs.and(isbnEqual(isbn));
+        }
+
+        if(titulo != null) {
+            specs = specs.and(tituloLike(titulo));
+        }
+
+        if(genero != null) {
+            specs = specs.and(generoEqual(genero));
+        }
+
+        if(anoPublicacao != null) {
+            specs = specs.and(anoPublicacaoEqual(anoPublicacao));
+        }
+
+        if(nomeAutor != null) {
+            specs = specs.and(nomeAutorLike(nomeAutor));
+        }
+
+        return repository.findAll(specs);
+    }
+
+    public void atualizar(Livro livro) {
+        if(livro.getId() == null) {
+            throw new IllegalArgumentException("Para atualizar, é necessário que o livro esteja salvo na base.");
+        }
+
+        repository.save(livro);
     }
 }
